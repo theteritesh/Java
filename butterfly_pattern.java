519626416f19d985aae6323084af1b8425e7c344
theteritesh <theteritesh2@gmail.com>
2023-12-23 10:47:36 +0530

    Java Practice

---
+++ butterfly_pattern.java
@@ -0,0 +1,37 @@
+public class butterfly_pattern{
+    public static void main(String[] args) {
+        //upper half
+        int n=4;
+        for(int i=1;i<=n;i++){
+            //1st part
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space=2*(n-i);
+            for (int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower part
+        for(int i=n;i>=1;i--){
+            //1st part
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            int space=2*(n-i);
+            for (int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            //2nd part
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
