21a358acd3389ca3bd2047cd18576a0988e2f449
theteritesh <theteritesh2@gmail.com>
2024-02-02 00:06:48 +0530

    This keyword practice

---
+++ Core_Java/thisandSuperKeywordPractice.java
@@ -0,0 +1,29 @@
+class A{
+    public A(){
+        super();
+        System.out.println("in A...");
+    }
+    public A(int b){
+        super();
+        System.out.println("in a int");
+    }
+
+}
+class B extends A{
+    public B(){
+        super();
+        System.out.println("in b...");
+    }
+
+    public B(int a){
+        this();
+        System.out.println("in b int");
+    }
+}
+public class thisandSuperKeywordPractice{
+    public static void main(String[] args) {
+        B Obj = new B(6);
+        // B Obj1 = new B();
+
+    }
+}
