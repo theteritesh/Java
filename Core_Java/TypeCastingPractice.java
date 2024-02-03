d0cd04f40bc26a38f72fd96ba7bc1557f7759d24
theteritesh <theteritesh2@gmail.com>
2024-02-03 14:55:59 +0530

    Type casting

---
+++ Core_Java/TypeCastingPractice.java
@@ -0,0 +1,22 @@
+class A{
+    public void show1(){
+        System.out.println("in A show");
+    }
+    
+}
+class B{
+    public void show2(){
+        System.out.println("in B show");
+    }
+}
+public class TypeCastingPractice{
+    public static void main(String[] args) {
+
+        A obj1 =new B();  //upcasting
+        obj1.show1();
+
+        B obj2=(B)obj1; //downcasting
+        obj2.show2();
+        
+    }
+}
