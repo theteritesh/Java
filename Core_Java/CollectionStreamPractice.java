afc4d2c0dcb5eaf0de07f34080170eb2a4a0f268
theteritesh <theteritesh2@gmail.com>
2024-02-14 23:53:41 +0530

    one file updated

--- Core_Java/CollectionStreamPractice.java
+++ Core_Java/CollectionStreamPractice.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+// import java.util.function.Function;
+// import java.util.function.Predicate;
+// import java.util.stream.Stream;
 
 public class CollectionStreamPractice {
     public static void main(String[] args) {
