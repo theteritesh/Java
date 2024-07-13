0aafec809881962daee6616c5332bd307c44bcfb
theteritesh <theteritesh2@gmail.com>
2024-07-14 00:12:39 +0530

    Practice

--- Core_Java/InputePractice2.java
+++ Core_Java/InputePractice2.java
@@ -6,8 +6,8 @@ public class InputePractice2 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         InputStreamReader in=new InputStreamReader(System.in);
         BufferedReader bf =new BufferedReader(in);
-        int num =Integer.parseInt(bf.readLine());
         System.out.println("Enter a Number : ");
+        int num =Integer.parseInt(bf.readLine());
         System.out.println(num);
 
         bf.close();
